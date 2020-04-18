ec53297d077f94632189c7d524f62ab4eadb06d6
Pawel Piotrowski <pawel@flytype.org>
2020-04-18 23:07:52 +0100

    add core module

---
+++ projects/covid19-stats-app/src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule
+  ]
+})
+export class CoreModule { }
