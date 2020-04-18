a75138cc55f9719f698f419c13c0778d4f40c0c1
Pawel Piotrowski <pawel@flytype.org>
2020-04-19 00:45:30 +0100

    add shared module

--- projects/covid19-stats-app/src/app/features/dashboard/dashboard.module.ts
+++ projects/covid19-stats-app/src/app/features/dashboard/dashboard.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { SharedModule } from '../../shared/shared.module';
 
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
-    CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    SharedModule
   ]
 })
 export class DashboardModule { }
