bb45cca5fd7b6cf26764c345844f63aa4936f0b7
Pawel Piotrowski <pawel@flytype.org>
2020-04-18 23:31:37 +0100

    add main layout core component

--- projects/covid19-stats-app/src/app/app.component.ts
+++ projects/covid19-stats-app/src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'covid19-stats-app';
-}
+export class AppComponent {}
--- projects/covid19-stats-app/src/app/core/core.module.ts
+++ projects/covid19-stats-app/src/app/core/core.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainLayoutComponent
+  ],
+  exports: [
+    MainLayoutComponent
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule
