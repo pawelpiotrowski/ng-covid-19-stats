9653b65936d425b5a8b6b05829a9b09eccb6f415
Pawel Piotrowski <pawel@flytype.org>
2020-04-19 00:37:39 +0100

    add simple app navigation

--- projects/covid19-stats-app/src/app/core/core.module.ts
+++ projects/covid19-stats-app/src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
@@ -15,6 +16,7 @@ import { MainLayoutComponent } from './layout/main-layout/main-layout.component'
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatButtonModule,
     MatToolbarModule,
     RouterModule
   ]
--- projects/covid19-stats-app/src/app/core/layout/main-layout/main-layout.component.spec.ts
+++ projects/covid19-stats-app/src/app/core/layout/main-layout/main-layout.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { MainLayoutComponent } from './main-layout.component';
@@ -12,6 +13,7 @@ describe('MainLayoutComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ MainLayoutComponent ],
       imports: [
+        MatButtonModule,
         MatToolbarModule,
         RouterTestingModule,
       ],
