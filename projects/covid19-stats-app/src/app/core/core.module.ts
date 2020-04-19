afd84e4e0ace1c513c171925e12e8e605b47d038
Pawel Piotrowski <pawel@flytype.org>
2020-04-19 01:09:20 +0100

    add flex layout

--- projects/covid19-stats-app/src/app/core/core.module.ts
+++ projects/covid19-stats-app/src/app/core/core.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
 @NgModule({
@@ -16,6 +18,7 @@ import { MainLayoutComponent } from './layout/main-layout/main-layout.component'
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MatButtonModule,
     MatToolbarModule,
     RouterModule
