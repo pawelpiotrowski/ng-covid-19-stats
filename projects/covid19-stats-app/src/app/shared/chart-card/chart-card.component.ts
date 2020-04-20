a7a1d092dd596ed21f70277605f37ea3b4da2c9b
Pawel Piotrowski <pawel@flytype.org>
2020-04-20 11:36:55 +0100

    clean up chart card

--- projects/covid19-stats-app/src/app/shared/chart-card/chart-card.component.ts
+++ projects/covid19-stats-app/src/app/shared/chart-card/chart-card.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { IChartOptions, IChartData } from '../chart/chart';
 
 @Component({
   selector: 'cvd-chart-card',
   templateUrl: './chart-card.component.html',
   styleUrls: ['./chart-card.component.scss']
 })
-export class ChartCardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ChartCardComponent {
+  @Input() title: string;
+  @Input() chartOptions: IChartOptions;
+  @Input() chartData: IChartData;
 }
