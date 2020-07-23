8edb78e30665d7f7e14fef7d67a3df30658a04ca
Pawel Piotrowski <pawel@flytype.org>
2020-07-23 17:10:31 +0200

    update data service mock and test

--- projects/covid19-stats-app/src/app/core/services/data/data.service.mock.ts
+++ projects/covid19-stats-app/src/app/core/services/data/data.service.mock.ts
@@ -1,20 +1,26 @@
 import { BehaviorSubject } from 'rxjs';
-import { IDataGlobalStats, IDataGlobalStatsUpdate, IDataTimelineStatsUpdate } from './data';
+import { IDataGlobalStatsUpdate, IDataTimelineStatsUpdate, IDataTimelineStats } from './data';
+import mockedGlobalStats from './mocks/global-stats.json';
 
-export const globalStatsUpdateDataMock: IDataGlobalStats = {
-  deaths: 10,
-  ill: 990,
-  infected: 3000,
-  recovered: 2000,
-  allStats: [],
-};
+export const timelineStatsUpdateMock: IDataTimelineStats = [{
+  active: 5554677,
+  confirmed: 15040502,
+  date: '2020-07-22',
+  deaths: 619505,
+  is_in_progress: true,
+  new_confirmed: 348397,
+  new_deaths: 9834,
+  new_recovered: 581808,
+  recovered: 8866320,
+  updated_at: '2020-07-22T10:22:22.512Z',
+}];
 
 export class DataServiceMock {
   getGlobalStatsUpdates(): BehaviorSubject<IDataGlobalStatsUpdate> {
-    return new BehaviorSubject(globalStatsUpdateDataMock);
+    return new BehaviorSubject({ allStats: (mockedGlobalStats.data as any) });
   }
 
   getTimelineStatsUpdates(): BehaviorSubject<IDataTimelineStatsUpdate> {
-    return new BehaviorSubject([]);
+    return new BehaviorSubject(timelineStatsUpdateMock);
   }
 }
--- projects/covid19-stats-app/src/app/core/services/data/data.service.spec.ts
+++ projects/covid19-stats-app/src/app/core/services/data/data.service.spec.ts
@@ -1,8 +1,12 @@
-import { TestBed, getTestBed } from '@angular/core/testing';
+import { TestBed, getTestBed, async } from '@angular/core/testing';
 
 import { DataService } from './data.service';
 import { BackendService } from '../backend/backend.service';
-import { BackendServiceMock, backendCountryLatestStatsDataMock } from '../backend/backend.service.mock';
+import {
+  BackendServiceMock,
+  backendCountryLatestStatsDataMock,
+  backendTimelineStatsDataMock,
+} from '../backend/backend.service.mock';
 
 describe('DataService', () => {
   let injector: TestBed;
@@ -26,21 +30,154 @@ describe('DataService', () => {
   });
 
   describe('getGlobalStatsUpdates', () => {
-    let backendSpy: any;
+    describe('when globalStats subject has no value', () => {
+      let backendSpy: any;
 
-    beforeEach(() => {
-      backendSpy = spyOn(backend, 'getCountriesLatestStats').and.callThrough();
+      beforeEach(() => {
+        backendSpy = spyOn(backend, 'getCountriesLatestStats').and.callThrough();
+      });
+
+      it('should call backend getCountriesLatestStats', () => {
+        service.getGlobalStatsUpdates();
+        expect(backendSpy).toHaveBeenCalled();
+      });
+
+      it('should return globalStats subject', () => {
+        const globalStats = service.getGlobalStatsUpdates();
+
+        expect(typeof globalStats.next).toEqual('function');
+      });
     });
 
-    it('should call backend getCountriesLatestStats', () => {
-      service.getGlobalStatsUpdates();
-      expect(backendSpy).toHaveBeenCalled();
+    describe('when globalStats subject has value', () => {
+      let backendSpy: any;
+
+      beforeEach(() => {
+        service.getGlobalStatsUpdates();
+        backendSpy = spyOn(backend, 'getCountriesLatestStats').and.callThrough();
+      });
+
+      it('should NOT call backend getCountriesLatestStats', () => {
+        service.getGlobalStatsUpdates();
+        expect(backendSpy).not.toHaveBeenCalled();
+      });
+
+      it('should return globalStats subject', () => {
+        const globalStats = service.getGlobalStatsUpdates();
+
+        expect(typeof globalStats.next).toEqual('function');
+      });
+    });
+  });
+
+  describe('getTimelineStatsUpdates', () => {
+    describe('when timelineStats subject has no value', () => {
+      let backendSpy: any;
+
+      beforeEach(() => {
+        backendSpy = spyOn(backend, 'getTimelineStats').and.callThrough();
+      });
+
+      it('should call backend getTimelineStats', () => {
+        service.getTimelineStatsUpdates();
+        expect(backendSpy).toHaveBeenCalled();
+      });
+
+      it('should return timelineStats subject', () => {
+        const timelineStats = service.getTimelineStatsUpdates();
+
+        expect(typeof timelineStats.next).toEqual('function');
+      });
     });
 
-    it('should update globalStats subject value', () => {
-      const globalStats = service.getGlobalStatsUpdates();
+    describe('when timelineStats subject has value', () => {
+      let backendSpy: any;
 
-      expect(globalStats.value?.allStats.length).toEqual(2);
+      beforeEach(() => {
+        service.getTimelineStatsUpdates();
+        backendSpy = spyOn(backend, 'getTimelineStats').and.callThrough();
+      });
+
+      it('should NOT call backend getTimelineStats', () => {
+        service.getTimelineStatsUpdates();
+        expect(backendSpy).not.toHaveBeenCalled();
+      });
+
+      it('should return globalStats subject', () => {
+        const timelineStats = service.getTimelineStatsUpdates();
+
+        expect(typeof timelineStats.next).toEqual('function');
+      });
+    });
+  });
+
+  describe('fetchGlobalStats', () => {
+    describe('when fetching is in progress', () => {
+      let backendSpy: any;
+
+      beforeEach(() => {
+        backendSpy = spyOn(backend, 'getCountriesLatestStats').and.callThrough();
+      });
+
+      it('should return early', () => {
+        service.fetchGlobalStats();
+        service.fetchGlobalStats();
+        expect(backendSpy).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    describe('when fetching is NOT in progress', () => {
+      let backendSpy: any;
+
+      beforeEach(() => {
+        backendSpy = spyOn(backend, 'getCountriesLatestStats').and.callThrough();
+      });
+
+      it('should update globalStats subject', async(() => {
+        service.getGlobalStatsUpdates().subscribe((updates) => {
+          // exclude null update
+          if (updates) {
+            expect(updates?.allStats.length).toEqual(backendCountryLatestStatsDataMock.length);
+            expect(service.getGlobalStatsUpdates().value?.allStats.length)
+              .toEqual(backendCountryLatestStatsDataMock.length);
+          }
+        });
+      }));
+    });
+  });
+
+  describe('fetchTimelineStats', () => {
+    describe('when fetching is in progress', () => {
+      let backendSpy: any;
+
+      beforeEach(() => {
+        backendSpy = spyOn(backend, 'getTimelineStats').and.callThrough();
+      });
+
+      it('should return early', () => {
+        service.fetchTimelineStats();
+        service.fetchTimelineStats();
+        expect(backendSpy).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    describe('when fetching is NOT in progress', () => {
+      let backendSpy: any;
+
+      beforeEach(() => {
+        backendSpy = spyOn(backend, 'getTimelineStats').and.callThrough();
+      });
+
+      it('should update timelineStats subject', async(() => {
+        service.getTimelineStatsUpdates().subscribe((updates) => {
+          // exclude null update
+          if (updates) {
+            expect(updates?.length).toEqual(backendTimelineStatsDataMock.length);
+            expect(service.getTimelineStatsUpdates().value?.length)
+              .toEqual(backendTimelineStatsDataMock.length);
+          }
+        });
+      }));
     });
   });
 });
